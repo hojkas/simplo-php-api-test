b36e37d045b6df639634e478c63c56e23960f474
hojkas <ivka.strnadova@gmail.com>
2022-06-21 19:55:15 +0200

    customer repository interface

---
+++ app/Interfaces/CustomerRepositoryInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Interfaces;
+
+interface CustomerRepositoryInterface
+{
+    public function getAllCustomers();
+    public function getCustomerById($customerId);
+    public function getCustomerByIdWithGroups($customerId);
+    public function deleteCustomer($customerId);
+    public function createCustomer(array $customerDetails);
+    public function updateCustomer($customerId, array $newDetails);
+}
