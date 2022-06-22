a36fee1b37acaae1609b573c5c0902391cb2122b
hojkas <ivka.strnadova@gmail.com>
2022-06-22 19:20:22 +0200

    customer group relationship

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Customer extends Model
 {
     use HasFactory;
+
+    public function groups()
+    {
+        return $this->belongsToMany(CustomerGroup::class);
+    }
 }
