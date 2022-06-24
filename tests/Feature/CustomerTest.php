d5def8dbeb2727c26dee670dba35a6acb727719b
hojkas <ivka.strnadova@gmail.com>
2022-06-24 22:42:33 +0200

    customers/ returns all customers test

--- tests/Feature/CustomerTest.php
+++ tests/Feature/CustomerTest.php
@@ -2,9 +2,12 @@
 
 namespace Tests\Feature;
 
+use App\Http\Resources\CustomerResource;
+use App\Models\Customer;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
 
 class CustomerTest extends TestCase
@@ -21,5 +24,18 @@ class CustomerTest extends TestCase
         $response = $this->get('api/customers/');
 
         $response->assertStatus(200);
+    }
+
+    public function test_all_two_customers_are_returned()
+    {
+        $customer1 = Customer::factory()->create();
+        $customer2 = Customer::factory()->create();
+
+        $response = $this->get('api/customers/');
+
+        $response->assertStatus(200);
+        $response->assertJsonCount(2);
+        $response->assertJsonFragment($customer1->toArray());
+        $response->assertJsonFragment($customer2->toArray());
     }
 }
