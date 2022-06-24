e78be239df6db73ae67b8b7e652c541007ed8c70
hojkas <ivka.strnadova@gmail.com>
2022-06-24 23:08:28 +0200

    test to customers/id to show and not show groups

--- tests/Feature/CustomerTest.php
+++ tests/Feature/CustomerTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use App\Http\Resources\CustomerResource;
 use App\Models\Customer;
+use App\Models\CustomerGroup;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -37,5 +38,33 @@ class CustomerTest extends TestCase
         $response->assertJsonCount(2);
         $response->assertJsonFragment($customer1->toArray());
         $response->assertJsonFragment($customer2->toArray());
+    }
+
+    public function test_correct_get_customer_with_groups()
+    {
+        $customer = Customer::factory()->create();
+        $group = CustomerGroup::factory()->create();
+        $customer->groups()->attach($group->id);
+
+        $customerA = $customer->toArray();
+
+        $response = $this->get("api/customers/{$customer->id}?showGroups=true");
+
+        $response->assertStatus(200);
+        $response->assertJsonFragment($customer->toArray());
+        $response->assertJsonFragment(['name' => $group->name]);
+    }
+
+    public function test_correct_get_customer_without_groups()
+    {
+        $customer = Customer::factory()->create();
+        $group = CustomerGroup::factory()->create();
+        $customer->groups()->attach($group->id);
+
+        $response = $this->get("api/customers/{$customer->id}");
+
+        $response->assertStatus(200);
+        $response->assertJsonFragment($customer->toArray());
+        $response->assertJsonMissing(['name' => $group->name]);
     }
 }
